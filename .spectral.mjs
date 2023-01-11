1b2e5823fb5631c3518c88a2aa975067b7173a63
jkawczynski7@gmail.com <jkawczynski7@gmail.com>
2023-01-11 13:14:55 +0100

    Modified 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE3NzQxNQ/spectral.js?branch_slug=main&token=38d47517-370c-4f8a-ba24-63e2fee1d657";
+export default { extends: ruleset };
